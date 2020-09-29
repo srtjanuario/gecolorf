df3556df14e96100ee5b86c3a4d5298f9c09c21b
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-09-29 11:37:24 -0300

    get set N

--- src/graph.h
+++ src/graph.h
@@ -4,23 +4,31 @@
 
 #include <vector>
 #include <numeric>
+#include <iostream>
 using namespace std;
 
 class Graph{
 	public:
-		Graph();	
+		Graph(int n);	
 		void Circle();
+
 		bool isPerfect();
 		bool isAbsolute();
+
+		void toHex();
+
+		int getN(){return N;};
+		void setN(int n){N=n;};
+
 	private:
 		int N;
 		int arestas;
+		
 
 	vector< vector<int> > table;
 	vector< vector<int> > vertexColor;
 	vector< vector<int> > lanterns;
 
-	
 };
 
 #endif
