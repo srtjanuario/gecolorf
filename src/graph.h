83465001191cc5d578a6fd4d3bcc27f461df8d1e
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-09-29 10:10:51 -0300

    vectors

--- src/graph.h
+++ src/graph.h
@@ -1,11 +1,18 @@
 #ifndef GECOLORF_GRAPH_H
 #define GECOLORF_GRAPH_H
 
+#include <vector>
+using namespace std;
+
 class Graph{
 	public:
 		Graph();	
 	private:
 		int nVertices;
+
+	vector< vector<int> > table;
+	vector< vector<int> > vertexColor;
+	vector< vector<int> > lanterns;
 };
 
 #endif
