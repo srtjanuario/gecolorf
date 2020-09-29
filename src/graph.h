a0dac4704104fdff4d254fbbaf9786164247946c
Tiago de Oliveira Januario <januarioccp@gmail.com>
2020-09-29 11:11:40 -0300

    function declarations

--- src/graph.h
+++ src/graph.h
@@ -1,18 +1,26 @@
 #ifndef GECOLORF_GRAPH_H
 #define GECOLORF_GRAPH_H
 
+
 #include <vector>
+#include <numeric>
 using namespace std;
 
 class Graph{
 	public:
 		Graph();	
+		void Circle();
+		bool isPerfect();
+		bool isAbsolute();
 	private:
-		int nVertices;
+		int N;
+		int arestas;
 
 	vector< vector<int> > table;
 	vector< vector<int> > vertexColor;
 	vector< vector<int> > lanterns;
+
+	
 };
 
 #endif
